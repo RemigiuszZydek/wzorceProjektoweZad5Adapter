aaebec37e4724fa9fc9f1b25fcffaf3965b1b7a5
Remik <zyduremik@gmail.com>
2024-03-24 10:09:37 +0100

    zad 5

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+delegate void Logger(string message);
+
+
+class IstniejacySystem {
+    public void Log(string message) {
+        Console.WriteLine("Log z istniejącego systemu: " + message);
+    }
+}
+
+class NowaBibliotekaRejestrowania {
+    public void LogMessage(string message) {
+        Console.WriteLine("Log z nowej biblioteki rejestrowania: " + message);
+    }
+}
+
+class LogAdapter {
+    private Logger logger;
+
+    public LogAdapter(Logger logger) {
+        this.logger = logger;
+    }
+
+    public void LogMessage(string message) {
+        logger(message);
+    }
+}
+
+
+class Program {
+    static void Main(string[] args) {
+        IstniejacySystem istniejacySystem = new IstniejacySystem();
+        NowaBibliotekaRejestrowania nowaBiblioteka = new NowaBibliotekaRejestrowania();
+
+        
+        LogAdapter adapter = new LogAdapter(istniejacySystem.Log);
+        adapter.LogMessage("Przykładowa wiadomość zarejestrowana za pomocą adaptera");
+
+      
+        adapter = new LogAdapter(nowaBiblioteka.LogMessage);
+        adapter.LogMessage("Inna wiadomość zarejestrowana za pomocą adaptera");
+    }
+}
